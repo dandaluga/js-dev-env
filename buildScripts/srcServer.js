fc41912ba7e51402ed524ae89701976e23c129e8
dandaluga <dan@daluga.com>
2016-11-20 16:16:30 -0600

    Additional code to mock getting data back from an API.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,6 +15,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 
+app.get('/users', function (req, res) {
+  res.json([
+    {"id": 1,"firstName":"Dan","lastName":"DaLuga","email":"dan@gmail.com"},
+    {"id": 2,"firstName":"Pam","lastName":"DaLuga","email":"pam@gmail.com"},
+    {"id": 3,"firstName":"Samantha","lastName":"DaLuga","email":"samantha@gmail.com"},
+    {"id": 4,"firstName":"Abel","lastName":"DaLuga","email":"abel@gmail.com"}
+  ]);
+});
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
